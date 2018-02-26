1131b5cbb0f0da73819f606c097a60f73b9c51a2
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-02-26 22:09:43 +0000

    sdd

--- server/ModelSQL.js
+++ server/ModelSQL.js
@@ -9,13 +9,13 @@ let sql;
 
 async function newTitle(title) {
   const sql = await init();
-  console.log("ok1")
+
 
   const insertQuery = sql.format('INSERT INTO headerNames (title) VALUES (?);', {title});
-  console.log("ok2")
+  console.log(title)
 
   await sql.query(insertQuery);
-  console.log("done")
+  //console.log("done")
 }
 
 
