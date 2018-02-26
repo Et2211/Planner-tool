42c85a2ab35b916c4ac45a327eae641db64e2e44
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-02-26 22:02:33 +0000

    fdsf

--- server/ModelSQL.js
+++ server/ModelSQL.js
@@ -7,11 +7,6 @@ const mysql = require('mysql2/promise');
 let sql;
 
 
-async function init() {
-  sql = await mysql.createConnection(config.mysql);
-
-}
-
 async function newTitle(title) {
   const sql = await init();
   console.log("ok1")
@@ -33,8 +28,37 @@ async function getHeaders(title) {
   const [rows] = await sql.query(query);
   console.log(rows)
   return rows;
+}
 
+async function init() {
+  if (sqlPromise) return sqlPromise;
 
+  sqlPromise = newConnection();
+  return sqlPromise;
+}
+
+async function shutDown() {
+  if (!sqlPromise) return;
+  const stashed = sqlPromise;
+  sqlPromise = null;
+  await releaseConnection(await stashed);
+}
+
+async function newConnection() {
+  // todo: this should really use connection pools
+  const sql = await mysql.createConnection(config.mysql);
+
+  // handle unexpected errors by just logging them
+  sql.on('error', (err) => {
+    console.error(err);
+    sql.end();
+  });
+
+  return sql;
+}
+
+async function releaseConnection(connection) {
+  await connection.end();
 }
 
 
@@ -42,6 +66,6 @@ module.exports = {
   init: init,
   newTitle:newTitle,
   getHeaders:getHeaders
-
+  shutDown: shutDown,
 
 }
