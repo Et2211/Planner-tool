7a1a7a6955f6da2b11295aa09dccf0545471d910
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-02-26 21:58:01 +0000

    json

--- server/ModelSQL.js
+++ server/ModelSQL.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const util = require('util');
-const config = require('./config');
+const config = require('./config.json');
 const mysql = require('mysql2/promise');
 let sql;
 
