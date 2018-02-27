25a622478ee44dc33e7dddb18dea141762796bc3
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-02-27 00:26:00 +0000

    space

--- server/ModelSQL.js
+++ server/ModelSQL.js
@@ -24,7 +24,7 @@ async function getHeaders(title) {
   const filter = '%' + title + '%';
   console.log(filter + "filter")
 
-    await sql.query('SELECT * FROM count FROM headerNames WHERE title = ?', [filter]);
+    await sql.query('SELECT * FROM count FROM headerNames WHERE title=?', [filter]);
 
   const [rows] = await sql.query(query);
   console.log(rows + + "rows")
