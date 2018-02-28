1ec4d1ce9795ae844a551b41abbf1dbee83c2ead
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-02-28 18:41:27 +0000

    //

--- webpages/index.js
+++ webpages/index.js
@@ -22,8 +22,8 @@ async function loadData() {
   const response = await fetch(url);
   if (response.ok) {
     console.log(await response.json()) // carry on from here!!!!!!
-    console.log(await response.json().["0"].coloumn1)
-  }  
+  //  console.log(await response.json().["0"].coloumn1)
+  }
   else {
     console.error('error getting', response.status, response.statusText);
 
