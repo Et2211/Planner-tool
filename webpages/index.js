25089f0a8b10cf0ff2e25937c30f7c4c1a0fa1a9
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-02-28 19:09:43 +0000

    awdwsa

--- webpages/index.js
+++ webpages/index.js
@@ -23,7 +23,7 @@ async function loadData() {
   if (response.ok) {
 
     let testMe = await response.json()
-    console.log(testMe) // carry on from here!!!!!!
+    console.log(JSON.stringify(testMe)) // carry on from here!!!!!!
 
   }
   else {
