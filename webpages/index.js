160b23d850f01b174beb4441e9f4209a8dcbd153
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-03-22 20:54:46 +0000

    sdf

--- webpages/index.js
+++ webpages/index.js
@@ -105,8 +105,9 @@ async function newPlan() {
 
   let newPlanName = window.prompt("Enter the name of your new plan");
   let numOfWeeks = window.prompt("Enter the number of weeks for this unit");
-  console.log(typeof (parseInt(numOfWeeks, 10)))
-  console.log(parseInt(numOfWeeks, 10))
+
+  numOfWeeks = parseInt(numOfWeeks, 10)
+
   if (Number.isInteger(numOfWeeks)) {
 
     const url = '/api/planner/new?name=' + encodeURIComponent(newPlanName) + "&weeks=" + encodeURIComponent(numOfWeeks);
