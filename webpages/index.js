6f660c43f16dc21c6a15e68e10b8d29209c10ee9
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-03-22 21:52:36 +0000

    float

--- webpages/index.js
+++ webpages/index.js
@@ -147,8 +147,8 @@ async function newPlan() {
   let newPlanName = window.prompt("Enter the name of your new plan");
   let numOfWeeks = window.prompt("Enter the number of weeks for this unit (Decimals will be rounded to the nearest integer)");
 
-  numOfWeeks = parseInt(numOfWeeks, 10)
-  numOfWeeks = Math.round(numOfWeeks)
+  numOfWeeks = parseFloat(numOfWeeks, 10);
+  numOfWeeks = Math.round(numOfWeeks);
 
   if (Number.isInteger(numOfWeeks)) {
 
